47225fef024adf3ddbb80edfc4db7f4124e2d492
Lakshay saini <lakshaysaini2013@gmail.com>
2021-02-17 11:46:08 +0530

    updated

--- controllers/Product.controller.js
+++ controllers/Product.controller.js
@@ -15,7 +15,7 @@ export const getProducts = asyncHandler(async (req, res) => {
     res.json(products);
   } else {
     const query = {};
-    let priceSort = "";
+    let priceSort = "-createdAt";
     if (range) {
       if (range === "ascending") {
         priceSort = "price";
@@ -67,14 +67,16 @@ export const deleteProduct = asyncHandler(async (req, res) => {
 // @route   POST /api/v1/products
 // @access  Private/Admin
 export const createProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, category } = req.body;
   const product = new Product({
-    name: "Sample name",
-    price: 0,
+    name: name || "Sample name",
+    price: price || 0,
     user: req.user._id,
     image:
-      "https://raw.githubusercontent.com/bradtraversy/proshop_mern/master/uploads/Screen%20Shot%202020-09-29%20at%205.50.52%20PM.png",
-    category: "Sample category",
-    description: "Sample description",
+      image ||
+      "https://www.netmeds.com/images/product-v1/600x600/14149/tentex_royal_capsule_10_s_0.jpg",
+    category: category || "medicine",
+    description: description || "Sample description",
   });
 
   const createdProduct = await product.save();
